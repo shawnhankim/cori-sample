e6f9efabe5b86402eb973c4566627ab502384216
Hancheol Kim <h.kim3@f5.com>
2019-01-16 14:34:43 -0800

    multiple parameters based sprintf for py

--- python/sprintf/sprintf.py
+++ python/sprintf/sprintf.py
@@ -3,3 +3,7 @@ print('The value of pi is approximately %5.3f.' % val)
 
 str_val = 'The value of pi is approximately %5.3f.' % val
 print(str_val)
+
+msg = "Test"
+str_val = 'The value of pi is approximately %5.3f (%s).' % (val, msg)
+print(str_val)
