026988dbdebea8e211ee424bc060d354bc7544fc
Hancheol Kim <h.kim3@f5.com>
2019-01-16 15:08:14 -0800

    space with sprintf for py

--- python/sprintf/sprintf.py
+++ python/sprintf/sprintf.py
@@ -4,6 +4,10 @@ print('The value of pi is approximately %5.3f.' % val)
 str_val = 'The value of pi is approximately %5.3f.' % val
 print(str_val)
 
-msg = "Test"
-str_val = 'The value of pi is approximately %5.3f (%s).' % (val, msg)
+msg = "Test 1"
+str_val = 'The value of pi is approximately %5.3f (%-10s) !!!' % (val, msg)
+print(str_val)
+
+msg = "Test 123"
+str_val = 'The value of pi is approximately %5.3f (%-10s) !!!' % (val, msg)
 print(str_val)
