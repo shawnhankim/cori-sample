1a2d12e81d19ecc34d61a04828aff1b955a40c70
Hancheol Kim <h.kim3@f5.com>
2019-02-20 14:43:53 -0800

    Update example of regex

--- go/regex/example_regex.go
+++ go/regex/example_regex.go
@@ -15,50 +15,53 @@ func main() {
 		fmt.Printf("Unable to compile a temporary regex\n")
 	}
 
+	fmt.Printf("\n1. Test Case : %s \n", `^\/ui\/.*`)
 	tmpURL := "/ui/user"
 	if uiRegex.MatchString(tmpURL) {
-		fmt.Printf("Mached 'ui' string in regex : %s\n", tmpURL)
+		fmt.Printf("MatchString() : Mached   'ui' string  in regex : %s\n", tmpURL)
 	} else {
-		fmt.Printf("Unmached 'ui' string in regex : %s\n", tmpURL)
+		fmt.Printf("MatchString() : Unmached 'ui' string  in regex : %s\n", tmpURL)
 	}
 	if uiRegex.Match([]byte(tmpURL)) {
-		fmt.Printf("Mached 'ui' pattern in regex : %s\n", tmpURL)
+		fmt.Printf("Match()       : Mached   'ui' pattern in regex : %s\n", tmpURL)
 	} else {
-		fmt.Printf("Unmached 'ui' pattern in regex : %s\n", tmpURL)
-	}
-	tmpURL1 := "/abcd/ui/user"
-	if uiRegex.MatchString(tmpURL1) {
-		fmt.Printf("Mached 'ui' string in regex : %s\n", tmpURL1)
-	} else {
-		fmt.Printf("Unmached 'ui' string in regex : %s\n", tmpURL1)
-	}
-	if uiRegex.Match([]byte(tmpURL1)) {
-		fmt.Printf("Mached 'ui' pattern in regex : %s\n", tmpURL1)
-	} else {
-		fmt.Printf("Unmached 'ui' pattern in regex : %s\n", tmpURL1)
+		fmt.Printf("Match()       : Unmached 'ui' pattern in regex : %s\n", tmpURL)
 	}
 
+	tmpURL1 := "/abcd/ui/user"
+	if uiRegex.MatchString(tmpURL1) {
+		fmt.Printf("MatchString() : Mached   'ui' string  in regex : %s\n", tmpURL1)
+	} else {
+		fmt.Printf("MatchString() : Unmached 'ui' string  in regex : %s\n", tmpURL1)
+	}
+	if uiRegex.Match([]byte(tmpURL1)) {
+		fmt.Printf("Match()       : Mached   'ui' pattern in regex : %s\n", tmpURL1)
+	} else {
+		fmt.Printf("Match()       : Unmached 'ui' pattern in regex : %s\n", tmpURL1)
+	}
+
+	fmt.Printf("\n2. Test Case : %s \n", `/downloads/*`)
 	tmpDownload := "/downloads/user"
 	if dnRegex.MatchString(tmpDownload) {
-		fmt.Printf("Mached 'downloads' string in regex : %s\n", tmpDownload)
+		fmt.Printf("MatchString() : Mached   'downloads' string  in regex : %s\n", tmpDownload)
 	} else {
-		fmt.Printf("Unmached 'downloads' string in regex : %s\n", tmpDownload)
+		fmt.Printf("MatchString() : Unmached 'downloads' string  in regex : %s\n", tmpDownload)
 	}
 	if uiRegex.Match([]byte(tmpDownload)) {
-		fmt.Printf("Mached 'downloads' pattern in regex : %s\n", tmpDownload)
+		fmt.Printf("Match()       : Mached   'downloads' pattern in regex : %s\n", tmpDownload)
 	} else {
-		fmt.Printf("Unmached 'downloads' pattern in regex : %s\n", tmpDownload)
+		fmt.Printf("Match()       : Unmached 'downloads' pattern in regex : %s\n", tmpDownload)
 	}
 
 	tmpDownload1 := "/abcd/downloads/user"
 	if dnRegex.MatchString(tmpDownload) {
-		fmt.Printf("Mached 'downloads' string in regex : %s\n", tmpDownload1)
+		fmt.Printf("MatchString() : Mached   'downloads' string  in regex : %s\n", tmpDownload1)
 	} else {
-		fmt.Printf("Unmached 'downloads' string in regex : %s\n", tmpDownload1)
+		fmt.Printf("MatchString() : Unmached 'downloads' string  in regex : %s\n", tmpDownload1)
 	}
 	if uiRegex.Match([]byte(tmpDownload)) {
-		fmt.Printf("Mached 'downloads' pattern in regex : %s\n", tmpDownload1)
+		fmt.Printf("Match()       : Mached   'downloads' pattern in regex : %s\n", tmpDownload1)
 	} else {
-		fmt.Printf("Unmached 'downloads' pattern in regex : %s\n", tmpDownload1)
+		fmt.Printf("Match()       : Unmached 'downloads' pattern in regex : %s\n", tmpDownload1)
 	}
 }
